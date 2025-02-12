e464f5a94ea56860a7db500f1ad7f06a6d718db1
ndugem45 <ndugem45@gmail.com>
2025-02-12 17:12:11 +0700

    add slider, add submission-news-stories page

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,32 @@
-// Put your application javascript here
+$(document).ready(() => {
+  $('.partner .container').slick({
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    variableWidth: true,
+    speed: 2500,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 10,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
+
+  $('.stories_slider').slick({
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.life_slider').slick({
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
