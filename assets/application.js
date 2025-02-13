76deb74b7c87fb78b7b9c178f567e8b00817d30b
ndugem45 <ndugem45@gmail.com>
2025-02-13 10:26:42 +0700

    add faq, footer

--- assets/application.js
+++ assets/application.js
@@ -29,4 +29,19 @@ $(document).ready(() => {
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+  const acc = document.getElementsByClassName('head');
+  let i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+      const panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      }
+    });
+  }
 });
